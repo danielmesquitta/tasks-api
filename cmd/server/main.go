58e8cddfd754a0122cbb080bfac3f0f588956aa8
Daniel Mesquita <danielmesquitta123@gmail.com>
2024-07-16 20:59:16 -0300

    add main server file

---
+++ cmd/server/main.go
@@ -0,0 +1,10 @@
+package main
+
+// @title Tasks API
+// @version 1.0
+// @description This is a CRUD API for tasks.
+// @contact.name Daniel Mesquita
+// @contact.email danielmesquitta123@gmail.com
+// @BasePath /api/v1
+func main() {
+}
